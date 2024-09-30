d2d8b6242012ac2b93109234d19bd199fed61d7d
WALWALASAFI <walwalasafi99@gmail.com>
2024-09-30 10:59:34 +0430

    add footer

---
+++ app/components/footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 p-4 text-center text-white">
+      &copy; {new Date().getFullYear()} E-commerce Store
+    </footer>
+  );
+}
