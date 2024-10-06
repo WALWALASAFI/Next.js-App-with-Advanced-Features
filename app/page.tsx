0b1b20905b11c902fe3e9d0ad110b7cf2097e290
WALWALASAFI <walwalasafi99@gmail.com>
2024-10-06 11:18:42 +0430

    add pic to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
 export default function HomePage() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold">Welcome to the W~Safi  Store</h1>
-      <p className="mt-2">Find the best clothes for you!</p>
+    <div className="flex flex-col min-h-screen justify-between items-center text-center">
+      <div className="flex-grow">
+        <h1 className="text-3xl font-bold">Welcome to the W~Safi Store</h1>
+        <p className="mt-5">Find the best clothes for you!</p>
+        <img 
+          src="image/images (6).jpg" // Replace with your image URL
+          alt="Clothing Store"
+          className="mt-5 w-1/2 h-auto rounded-lg shadow-lg" 
+        />
+      </div>
     </div>
   );
 }
--- app/page/products/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold">Welcome to the W~Safi  Store</h1>
-      <p className="mt-2">Find the best clothes for you!</p>
-    </div>
-  );
-}
