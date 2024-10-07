f0e6f619ebeb77ca47727e964e896a66b81b92fe
WALWALASAFI <walwalasafi99@gmail.com>
2024-10-07 09:18:27 +0430

    import new file products.js to fetch the data

---
+++ app/products.js
@@ -0,0 +1,15 @@
+// app/products/products.js
+export const products = [
+  {
+    id: 1,
+    title: "Product 1",
+    image: "image1.jpg",
+    price: 100,
+  },
+  {
+    id: 2,
+    title: "Product 2",
+    image: "image2.jpg",
+    price: 150,
+  },
+];
